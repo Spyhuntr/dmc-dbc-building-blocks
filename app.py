f5846278a6462a35c7600fa6517f25dd2946c114
tim <tim.j.phillips10@gmail.com>
2022-12-07 22:18:17 -0500

    Removed debug mode

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ server = app.server
 
 if __name__ == '__main__':
     app.run_server(
-        host='0.0.0.0',
-        port='8050',
-        debug=True,
-        dev_tools_props_check=True
+        # host='0.0.0.0',
+        # port='8050',
+        # debug=True,
+        # dev_tools_props_check=True
     )
